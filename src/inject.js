6f37aabf81d59815c509cc638a2bf850a3f98039
Paul Shorey <ps@artspaces.net>
2021-08-03 09:59:08 -0500

    amazing! GSearch now so much better!

--- src/inject.js
+++ src/inject.js
@@ -51,14 +51,15 @@
   // debug
   if (DEBUG1) console.clear()
 
-  let IS_GOOGLE_SEARCH = window.location.href.includes("google.") && window.location.pathname.includes("/search")
+  let IS_GOOGLE = window.location.href.includes("google.")
 
 
   /*
    * RUN FLY SWATTER
    */
-  if (!IS_GOOGLE_SEARCH) {
-    // on page load, as soon as elements are loaded, destroy them!
+  // on page load, as soon as elements are loaded, destroy them!
+  if (IS_GOOGLE && window.location.pathname.includes("/search")) {
+  } else {
     setTimeout(removeAnnoyingAnnoyances.bind({ timeout: 0 }), 0)
     setTimeout(removeAnnoyingAnnoyances.bind({ timeout: 1000 }), 1000)
     setTimeout(removeAnnoyingAnnoyances.bind({ timeout: 2500 }), 2500)
@@ -70,10 +71,13 @@
   /*
    * RUN GOOGLE
    */
-  if (IS_GOOGLE_SEARCH) {
-    setTimeout(fixGoogle.bind({ timeout: 0 }), 0)
-    setTimeout(fixGoogle.bind({ timeout: 1000 }), 1000)
-    setTimeout(fixGoogle.bind({ timeout: 2000 }), 2000)
+  if (IS_GOOGLE && window.location.pathname.includes("/search")) {
+    setTimeout(fixGoogleSearch.bind({ timeout: 0 }), 0)
+    setTimeout(fixGoogleSearch.bind({ timeout: 1000 }), 1000)
+  }
+  if (IS_GOOGLE && window.location.pathname.includes("/preferences")) {
+    setTimeout(fixGooglePreferences.bind({ timeout: 0 }), 0)
+    setTimeout(fixGooglePreferences.bind({ timeout: 1000 }), 1000)
   }
 
   /*
@@ -569,22 +573,26 @@
     }
   }
 
-  function fixGoogle() {
+  function fixGooglePreferences() {
+    let buttons = window.document.querySelector("#form-buttons")
+    if (buttons) {
+      buttons.style.position = "fixed"
+      buttons.style.top = "78px"
+    }
+  }
+
+  function fixGoogleSearch() {
+
     // open external links in new tab
-    let elems = window.document.body.querySelectorAll("#search a")
-    let len = elems.length
-    for (let i = 0; i < len; i++) {
-      let link = elems[i]
+    let links = Array.from(window.document.body.querySelectorAll("#search a"))
+    for (let link of links) {
       if (!link.dataset.ved) continue
       link.dataset.ved = ""
       link.removeAttribute("data-ved", "")
       link.removeAttribute("ping", "")
       link.setAttribute("target", "_blank")
-      // fix the link
-      if (!window.link) {
-        window.link = link
-      }
     }
+    let num_search_results = Array.from(window.document.body.querySelectorAll("#search h3")).length
     // hide ads and annoying content
     // let selectors = ['#taw','#botstuff','#bottomads']
     let botstuff = window.document.querySelector("#botstuff")
@@ -594,15 +602,30 @@
     let bads = window.document.querySelector("#bottomads")
     if (bads) {
       bads.style.opacity = "0.33"
+      bads.style.display = "none"
     }
     let tads = window.document.querySelector("#taw")
     if (tads) {
       tads.style.opacity = "0.33"
+      bads.style.display = "none"
+    }
+    let adlists = window.document.querySelectorAll("#taw [role=\"list\"], #taw [role=\"list\"]")
+    for (let adlist of Array.from(adlists)) {
+      if (adlist) {
+        // adlist.style.opacity = "0.33"
+        adlist.style.display = "none"
+      }
     }
     let topStoriesTitle = document.querySelector("title-with-lhs-icon")
     if (topStoriesTitle) {
       try {
         topStoriesTitle.parentElement.parentElement.parentElement.style.display = "none"
+      } catch (e) {}
+    }
+    let localResult = document.querySelector("g-img")
+    if (localResult) {
+      try {
+        localResult.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.display = "none"
       } catch (e) {}
     }
     let accordions = window.document.querySelectorAll("g-accordion-expander")
@@ -659,11 +682,30 @@
       // gel.previousElementSibling.style.marginBottom = "30px"
     }
 
-
     let extabar = window.document.querySelector("#extabar")
+    let stats = window.document.querySelector("#result-stats")
     if (extabar) {
-      extabar.style.opacity = "0"
-      extabar.style.height = "30px"
+      if (num_search_results >= 20) {
+        // OK good number of results
+        // if (stats) {
+        //   stats.innerHTML = num_search_results
+        // }
+        extabar.style.opacity = "0"
+        extabar.style.height = "30px"
+      } else {
+        extabar.style.opacity = "0.67"
+        extabar.style.height = "60px"
+        // HELP the person set more than 10 results
+        if (stats) {
+          stats.innerHTML = `
+          <div>
+<!--            <div><b>You're currently only getting 10 search results at a time!</b></div>-->
+            Open <a href="https://www.google.com/preferences">Google Preferences</a> to enable more than 10 results on each page.
+          </div>
+        `
+        }
+
+      }
     }
   }
 
